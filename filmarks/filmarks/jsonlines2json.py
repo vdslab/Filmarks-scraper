594fd4fb6e2d81ca92ac0ac7d30e30dbbb833393
FugaTakata <maple1213goo@gmail.com>
2021-11-01 10:32:48 +0900

    update

--- filmarks/filmarks/jsonlines2json.py
+++ filmarks/filmarks/jsonlines2json.py
@@ -3,10 +3,18 @@ import json
 
 def main():
     data = []
+    occupations = set()
+    genres = set()
+    countries = set()
     with open('./filmarks.jsonlines', mode='r') as f:
         for line in f.readlines():
             obj = json.loads(line)
-
+            for o in obj['production_members'].keys():
+                occupations.add(o)
+            for g in obj['genres']:
+                genres.add(g)
+            for c in obj['production_countries']:
+                countries.add(c)
             data.append(obj)
     # with open('./filmarks_fix.jsonlines', mode='w') as f:
     #     for d in data:
@@ -14,6 +22,12 @@ def main():
     #         f.write(json.dumps(d, ensure_ascii=False) + '\n')
 
     with open('./filmarks.json', mode='w') as f:
+        occupations = list(occupations)
+        genres = list(genres)
+        countries = list(countries)
+        print(occupations, genres, countries)
+        data = {'occupations': occupations,
+                'genres': genres, 'countires': countries, 'movies': data}
         json.dump(data, f, ensure_ascii=False)
 
 
